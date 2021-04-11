e4c47b11719924705edc09c6e26c31520136488c
Pedro Valentinuci <30358845+PedroValentinuci@users.noreply.github.com>
2021-04-11 16:21:54 -0300

    less lines

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,28 +1,9 @@
 var d = new Date();
 
-var month = new Array(12);
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var m = month[d.getDay()];
 
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 var w = weekday[d.getDay()]
 
 console.log("Day: " + d.getDate());
